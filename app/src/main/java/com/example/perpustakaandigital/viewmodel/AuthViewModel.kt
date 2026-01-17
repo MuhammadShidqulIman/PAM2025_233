eda95df0c2a0fa5bd58daec6129da1b0cd820502
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2026-01-17 16:38:15 +0700

    Update validasi register

--- app/src/main/java/com/example/perpustakaandigital/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/perpustakaandigital/viewmodel/AuthViewModel.kt
@@ -7,7 +7,19 @@ import com.example.perpustakaandigital.data.model.User
 import kotlinx.coroutines.launch
 
 class AuthViewModel(private val userDao: UserDao) : ViewModel() {
+
     fun register(username: String, password: String, nama: String, onResult: (Boolean, String) -> Unit) {
+
+        if (username.isBlank() || password.isBlank() || nama.isBlank()) {
+            onResult(false, "Semua data wajib diisi!")
+            return
+        }
+
+        if (username.length < 8 || password.length < 8) {
+            onResult(false, "Username & Password minimal 8 karakter!")
+            return
+        }
+
         viewModelScope.launch {
             val adaUser = userDao.cekUsername(username)
             if (adaUser != null) {
@@ -19,8 +31,12 @@ class AuthViewModel(private val userDao: UserDao) : ViewModel() {
             }
         }
     }
-
     fun login(username: String, password: String, onResult: (Boolean, User?) -> Unit) {
+
+        if (username.isBlank() || password.isBlank()) {
+            onResult(false, null)
+            return
+        }
         viewModelScope.launch {
             val user = userDao.login(username, password)
             if (user != null) {
